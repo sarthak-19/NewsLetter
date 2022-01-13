0fc8e45dd6b900c4d31cd4d67bc52bdbb14d331f
sarthak-19 <sarthak.8858@gmail.com>
2022-01-13 23:44:29 +0530

    Completed

---
+++ app.js
@@ -0,0 +1,75 @@
+//jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post('/', function (req, res) 
+{
+    var fname = req.body.firstName;
+    var lname = req.body.lastName;
+    var email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    }
+
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us20.api.mailchimp.com/3.0/lists/5a7aaf2b25"
+    const options = {
+        method: "POST",
+        auth: "sarthak-19:XX43125e2be0837d7afc0e8b7478adfe0f-us20"
+    }
+    const request = https.request(url, options, function (response) 
+    {   
+        if(response.statusCode===200)
+        {
+            res.sendFile(__dirname+"/success.html");
+        }
+        else
+        {
+            res.sendFile(__dirname+"/failure.html");
+        }
+        response.on("data", function (data) {
+        console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
+    // console.log(fname, lname, email)
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
+    console.log('Listening on 3000')
+});
+
+//api key
+// 43125e2be0837d7afc0e8b7478adfe0f-us20
+
+// list id
+// 5a7aaf2b25
